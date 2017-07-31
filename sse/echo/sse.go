2445e80dda806753c9a83af4fee4ddbd97aecb40
Bartosz Gołek <bartosz.golek@pearson.com>
2017-07-31 14:14:12 +0200

    sse - exiting http stream writing loop on stream close

--- sse/echo/sse.go
+++ sse/echo/sse.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"fmt"
+
 	"github.com/labstack/echo"
 	"github.com/open-gtd/server/sse"
 )
@@ -12,12 +14,14 @@ func NewSseRegisterer(g *echo.Group) sse.SseRegisterer {
 	return &sseRegisterer{
 		group:   g,
 		channel: make(chan interface{}),
+		exit:    make(chan bool),
 	}
 }
 
 type sseRegisterer struct {
 	group   *echo.Group
 	channel chan interface{}
+	exit    chan bool
 }
 
 func (sr sseRegisterer) CreatePushDataFunc(prefix sse.Prefix, closeNotify sse.ClientClosedNotificationFunc) sse.PushDataToSseFunc {
@@ -29,17 +33,27 @@ func (sr sseRegisterer) CreatePushDataFunc(prefix sse.Prefix, closeNotify sse.Cl
 
 		go func() {
 			<-notify
+			fmt.Println("notify")
+
+			sr.exit <- true
 			closeNotify()
 		}()
 
 		for {
-			v := <-sr.channel
-			if err := json.NewEncoder(c.Response()).Encode(v); err != nil {
-				return err
+			fmt.Println("new loop iteratinon")
+			select {
+			case <-sr.exit:
+				fmt.Println("exit")
+				return nil
+			case v := <-sr.channel:
+				if err := json.NewEncoder(c.Response()).Encode(v); err != nil {
+					return err
+				}
+				c.Response().Flush()
 			}
-			c.Response().Flush()
 		}
 
+		fmt.Println("exit")
 		closeNotify()
 
 		return nil
