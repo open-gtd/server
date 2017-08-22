964c652f9de8a63c413772259db61442e92c4bed
Bartosz Gołek <bartosz.golek@gmail.com>
2017-08-22 13:17:57 +0200

    registerer provider test

---
+++ api/provider_test.go
@@ -0,0 +1,33 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetRegisterer(t *testing.T) {
+	registerer := TestRegisterer{}
+	SetRegisterer(registerer)
+	assert.Equal(t, registerer, r)
+}
+
+func TestDefaultRegisterer(t *testing.T) {
+	assert.IsType(t, NullRegisterer{}, r)
+}
+
+func TestGetRegisterer(t *testing.T) {
+	registerer := TestRegisterer{}
+	r = registerer
+
+	assert.Equal(t, registerer, GetRegisterer())
+}
+
+type TestRegisterer struct {
+}
+
+func (TestRegisterer) GET(prefix string, path string, handlerFunc HandlerFunc)    {}
+func (TestRegisterer) PATCH(prefix string, path string, handlerFunc HandlerFunc)  {}
+func (TestRegisterer) POST(prefix string, path string, handlerFunc HandlerFunc)   {}
+func (TestRegisterer) PUT(prefix string, path string, handlerFunc HandlerFunc)    {}
+func (TestRegisterer) DELETE(prefix string, path string, handlerFunc HandlerFunc) {}
