2af4b1b97d5e7dea5175c66741855f4c64d4c63f
Bartosz Gołek <bartosz.golek@gmail.com>
2017-08-29 09:40:01 +0200

    first handlong tests with correct return handling

--- api/handling_test.go
+++ api/handling_test.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"errors"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/mock"
@@ -8,11 +10,13 @@ import (
 
 func TestHandleRequest_ShouldRunControllerFromFactory(t *testing.T) {
 	controller := TestController{}
+	fac := TestFactory{}
 
-	controller.On("Run").Once()
+	controller.On("Run").Once().Return(nil, nil)
+	fac.On("Create").Return(controller, nil)
 
 	HandleRequest(
-		func (Request, Response) (Controller, ControllerDestroyFunc, error) {
+		func(Request, Response) (Controller, ControllerDestroyFunc, error) {
 			return controller, nil, nil
 		},
 		TestRequest{},
@@ -22,33 +26,100 @@ func TestHandleRequest_ShouldRunControllerFromFactory(t *testing.T) {
 	controller.AssertExpectations(t)
 }
 
+func TestHandleRequest_ShouldCallDestroyFunction(t *testing.T) {
+	controller := TestController{}
+	fac := TestFactory{}
+
+	controller.On("Run").Once().Return(nil, nil)
+	fac.On("Create").Return(controller, nil)
+	fac.On("Destroy").Once().Return(nil)
+
+	HandleRequest(
+		fac.Create,
+		TestRequest{},
+		TestResponse{},
+	)
+
+	controller.AssertExpectations(t)
+}
+
+func TestHandleRequest_ShouldCallDestroyFunctionEvenIfFactoryReturnsError(t *testing.T) {
+	controller := TestController{}
+	fac := TestFactory{}
+
+	fac.On("Create").Return(nil, errors.New("Some error"))
+	fac.On("Destroy").Once().Return(nil)
+
+	HandleRequest(
+		fac.Create,
+		TestRequest{},
+		TestResponse{},
+	)
+
+	controller.AssertExpectations(t)
+}
+
 type TestController struct {
 	mock.Mock
-
-	e error
 }
 
 func (t TestController) Run() error {
-	t.Called()
-	return t.e
+	args := t.Called()
+	return args.Error(0)
 }
 
 type TestResponse struct {
 	mock.Mock
-
-	e error
 }
 
-func (t TestResponse) String(code int, s string) error    { return t.e }
-func (t TestResponse) JSON(code int, i interface{}) error { return t.e }
-func (t TestResponse) NoContent(code int) error           { return t.e }
+func (t TestResponse) String(code int, s string) error {
+	args := t.Called(code, s)
+	return args.Error(0)
+}
+func (t TestResponse) JSON(code int, i interface{}) error {
+	args := t.Called(code, i)
+	return args.Error(0)
+}
+func (t TestResponse) NoContent(code int) error {
+	args := t.Called(code)
+	return args.Error(0)
+}
 
 type TestRequest struct {
 	mock.Mock
-
-	name string
-	e    error
 }
 
-func (t TestRequest) Param(name string) string { return t.name }
-func (t TestRequest) Bind(i interface{}) error { return t.e }
+func (t TestRequest) Param(name string) string {
+	args := t.Called(name)
+	return args.String(0)
+}
+func (t TestRequest) Bind(i interface{}) error {
+	args := t.Called(i)
+	return args.Error(0)
+}
+
+type TestFactory struct {
+	mock.Mock
+}
+
+func (t TestFactory) Create(Request, Response) (Controller, ControllerDestroyFunc, error) {
+	args := t.Called()
+	return controller(args.Get(0)), t.Destroy, args.Error(1)
+}
+
+func controller(obj interface{}) Controller {
+	var c Controller
+	var ok bool
+	if obj == nil {
+		return nil
+	}
+	if c, ok = obj.(Controller); !ok {
+		panic(fmt.Sprintf("assert: arguments: Controller failed because object wasn't correct type: %v", obj))
+	}
+	return c
+}
+
+func (t TestFactory) Destroy() error {
+	args := t.Called()
+	return args.Error(0)
+}
